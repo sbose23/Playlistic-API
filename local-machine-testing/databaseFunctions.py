23dcaa43fde0354948cdb6fdb117b4214d3c3f09
Sudipto Bose <sudipto.bose23@gmail.com>
2022-08-16 20:35:23 -0400

    updated local machine dev scripts

--- local-machine-testing/databaseFunctions.py
+++ local-machine-testing/databaseFunctions.py
@@ -1,5 +1,6 @@
 #import python AWS SDK
 import boto3
+from boto3.dynamodb.conditions import Key
 
 #access DynamoDB resource running at this endpoint on Docker
 db = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
@@ -13,6 +14,7 @@ def createTable():
     Creates DynamoDB table called 'playlistic-local' with Partion(Hash) key 'userID'
     and Sort(Range) key 'playlistID'
     """
+
     db.create_table(TableName='playlistic-local',
                     AttributeDefinitions=[{'AttributeName': 'userID', 'AttributeType': 'S'},
                     {'AttributeName': 'playlistID','AttributeType': 'S'}],
@@ -24,13 +26,21 @@ def createTable():
 
 def initializeTable():
     """
-    Initializes the table created in createTable() with 5 unique rows
+    Initializes the table created in createTable() with 3 unique rows
     """
 
-    for i in range(5):
+    for i in range(3):
         videos = ["A" + str(i), "B" + str(i), "C" + str(i), "D" + str(i)]
         input = {'userID': 'User' + str(i), 'playlistID': 'Playlist' + str(i), 'videos' : videos}
         table.put_item(Item=input)
+
+    #duplicates of 0 for testing/development
+    videos1 = ["A" + str(1), "B" + str(1), "C" + str(1), "D" + str(1)]
+    videos2 = ["A" + str(2), "B" + str(2), "C" + str(2), "D" + str(2)]
+    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(1), 'videos' : videos1}
+    table.put_item(Item=input)
+    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(2), 'videos' : videos2}
+    table.put_item(Item=input)
     print("Initialized Table")
 
 
@@ -44,3 +54,12 @@ def scanTable():
     for row in result['Items']:
         print(str(row) + '\n')
     print("-------------------------------------------\nFinished printing database\n")
+
+def getUserPlaylists(user):
+    """
+    Return all playlists belonging to some user
+    """
+
+    response = table.query(KeyConditionExpression=Key('userID').eq(user))
+    print("Response: " + str(response))
+    return response
--- local-machine-testing/test.py
+++ local-machine-testing/test.py
@@ -6,6 +6,15 @@ import databaseFunctions as db
 
 print("-------------------------------------------\nStart of operations:\n")
 
-db.createTable()
-db.initializeTable()
-db.scanTable()
+def makeTable():
+    db.createTable()
+    db.initializeTable()
+    db.scanTable()
+
+def printUserPlaylists():
+    response = db.getUserPlaylists("User1")
+    print("-------------------------------------------\nPrinting playlists...\n")
+    for playlist in response['Items']:
+        print(str(playlist['playlistID']) + ": " + str(playlist['videos']))
+    print("-------------------------------------------\nFinished printing playlists.\n")
+
