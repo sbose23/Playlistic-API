9eb85527b150992866009612de7b90624db47aeb
Sudipto Bose <sudipto.bose23@gmail.com>
2022-08-16 21:55:11 -0400

    improved/added error/bad headers handling

--- apiEndpoints/getPlaylist/lambda_function.py
+++ apiEndpoints/getPlaylist/lambda_function.py
@@ -9,14 +9,34 @@ def lambda_handler(event, context):
     """
     This lambda function returns a playlist given userID and playlistID
     """
-    userID = event['headers']['userID']
-    playlistID = event['headers']['playlistID']
+
+    #check if required headers are present
+    if 'userID' in event['headers']:
+        userID = event['headers']['userID']
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Header 'userID' not present in request")
+        }
+
+    if 'playlistID' in event['headers']:
+        playlistID = event['headers']['playlistID']
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Header 'playlistID' not present in request")
+        }
+
+    #query database with userID (partion key) and playlistID (sort key)
     response = table.query(KeyConditionExpression=Key('userID').eq(userID)&Key('playlistID').eq(playlistID))
+
+    #if no playlists are present
     if len(response['Items']) == 0:
         return{
             'statusCode': 400,
             'body': json.dumps("No playlist exists with userID " + userID + " and " + "playlistID " + playlistID)
         }
+    
     return {
         'statusCode': 200,
         'body': json.dumps(response['Items'][0]['videos'])
--- apiEndpoints/getUserPlaylists/lambda_function.py
+++ apiEndpoints/getUserPlaylists/lambda_function.py
@@ -9,11 +9,33 @@ def lambda_handler(event, context):
     """
     This lambda function returns playlists belonging to some user
     """
+
+    #playlists dictionary to return to client
     playlists = {}
-    userID = event['headers']['userID']
+
+    #check userID header present
+    if 'userID' in event['headers']:
+        userID = event['headers']['userID']
+    else:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Header 'userID' not present in request")
+        }
+    
+    #query database with userID (partition key)
     response = table.query(KeyConditionExpression=Key('userID').eq(userID))
+
+    #if no playlists are found
+    if len(response['Items']) == 0:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("No playlists found for user with userID " + userID)
+        }
+    
+    #loop through Items and extract playlists with playlistID and corresponding videos list
     for playlist in response['Items']:
         playlists[playlist['playlistID']] = playlist['videos']
+
     return {
         'statusCode': 200,
         'body': json.dumps(playlists)
