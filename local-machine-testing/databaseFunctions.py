aa6b6a349d3e877d8a08270efbea5e33bfc0e1aa
Sudipto Bose <sudipto.bose23@gmail.com>
2022-08-17 15:34:04 -0400

    added add/update/delete functionality in local dev

--- local-machine-testing/databaseFunctions.py
+++ local-machine-testing/databaseFunctions.py
@@ -31,15 +31,15 @@ def initializeTable():
 
     for i in range(3):
         videos = ["A" + str(i), "B" + str(i), "C" + str(i), "D" + str(i)]
-        input = {'userID': 'User' + str(i), 'playlistID': 'Playlist' + str(i), 'videos' : videos}
+        input = {'userID': 'User' + str(i), 'playlistID': 'Playlist' + str(i), 'videos' : videos, 'playlistName': 'Unnamed'}
         table.put_item(Item=input)
 
     #duplicates of 0 for testing/development
     videos1 = ["A" + str(1), "B" + str(1), "C" + str(1), "D" + str(1)]
     videos2 = ["A" + str(2), "B" + str(2), "C" + str(2), "D" + str(2)]
-    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(1), 'videos' : videos1}
+    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(1), 'videos' : videos1, 'playlistName': 'Unnamed'}
     table.put_item(Item=input)
-    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(2), 'videos' : videos2}
+    input = {'userID': 'User' + str(0), 'playlistID': 'Playlist' + str(2), 'videos' : videos2, 'playlistName': 'Unnamed'}
     table.put_item(Item=input)
     print("Initialized Table")
 
@@ -55,6 +55,7 @@ def scanTable():
         print(str(row) + '\n')
     print("-------------------------------------------\nFinished printing database\n")
 
+
 def getUserPlaylists(user):
     """
     Return all playlists belonging to some user
@@ -64,11 +65,32 @@ def getUserPlaylists(user):
     print("Response: " + str(response))
     return response
 
+
 def getPlaylist(user, playlist):
     """
     Gets playlist given userID and playlistID
     """
-
     response = table.query(KeyConditionExpression=Key('userID').eq(user)&Key('playlistID').eq(playlist))
     print("Response: " + str(response))
     return response
+
+
+def addPlaylist(user, playlist, videosString, name):
+    """
+    Adds playlist given userID, playlistID/Name, and videos as a string with a hyphen delimiiter
+    """
+    videos = []
+    if len(videosString) > 0:
+        for video in videosString.split('-'):
+            videos.append(video)         
+    playlist = {'userID': user, 'playlistID': playlist, 'videos': videos, 'playlistName': name}
+    table.put_item(Item=playlist)
+    print("Added row: " + str(playlist))
+
+
+def deletePlaylist(user, playlist):
+    """
+    Deletes playlist given userID and playlistID
+    """
+    table.delete_item(Key={'userID': user, 'playlistID': playlist})
+    print("Deleted playlist " + playlist + " successfully")
